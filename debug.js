146a9d3ebb49f216a7591f64dccf4ada36d0873c
Pro-Use <robertprouse@gmail.com>
2023-08-09 21:44:35 +0100

    add debug popup

---
+++ debug.js
@@ -0,0 +1,82 @@
+//Background comms
+ var port = browser.runtime.connect({
+ });
+ // Heartbeat command to keep background alive
+setInterval( () => { port.postMessage("keep_alive") }, 10000)
+
+// const base_url = "https://plugin.arebyte.com/";
+const base_url = "https://dev.10pm.studio/arebyte-ext/"
+// const base_url = "http://localhost/wrong/"
+
+// get existing storage
+
+const get_info = async () => {
+  let result = await browser.storage.local.get(['lastUpdate', 'paused', 'selfUpdated', 'project', 'history'])
+  console.log('result', result)
+  // get popups
+  let project_url
+  if (result.project && result.project.slug && result.project.day){
+      project_url = base_url+'invites.json?project='+result.project.slug+'&day='+result.project.day
+  } else  {
+      project_url = base_url+'invites.json'
+  }
+  let response = await fetch(project_url, {mode: 'cors'})
+  if (response.status !== 200) {
+    console.log('Looks like there was a problem. Status Code: ' +
+      response.status);
+    return;
+  }
+  // Examine the text in the response
+  let data = await response.json()
+  console.log(data)
+  if (result.project.slug){
+
+    let projectCell = document.getElementById('project-cell').innerHTML = result.project.slug
+
+    document.getElementById('days-cell').innerHTML = data.days
+
+    document.getElementById('cur-days-cell').innerHTML = result.project.day
+
+    document.getElementById("new-day").setAttribute("max", data.days);
+
+    let changeDay = document.getElementById("change-day");
+    changeDay.addEventListener('click', async (e) => {
+      e.preventDefault()
+      let newDay = document.getElementById('new-day')
+      console.log("Going to " + newDay.value)
+      result.project.day = newDay.value
+      if (newDay.value == 1){
+        let new_ts = new Date().getTime()
+        result.project.start = new_ts
+      }
+      console.log(result.project)
+      await browser.storage.local.set({project: result.project})
+      port.postMessage('refresh')
+
+    })
+
+  }
+
+
+  document.getElementById('width-cell').innerHTML = window.screen.availWidth
+  document.getElementById('height-cell').innerHTML = window.screen.availHeight
+
+  let { history } = await browser.storage.local.get('history')
+
+  if (history && Array.isArray(history)){
+    let history_str = history.join(', ')
+    document.getElementById('history-cell').innerHTML = history_str
+  }
+
+  let clearHistory = document.getElementById('clear-history');
+  clearHistory.addEventListener('click', async (e) => {
+    e.preventDefault()
+    await browser.storage.local.remove('history')
+    port.postMessage('refresh')
+    document.getElementById('history-cell').innerHTML = ''
+  })
+
+
+}
+
+get_info()
